baf95bc78eb4c4b2789e7b4fe62d12c51d51915c
servosoftonline <oscarfernandezsantiago@gmail.com>
2023-10-02 10:37:13 +0200

    Aprendiendo Javascript

--- tutoriales/Javascript/medio-avanzado/Proyectos/Tienda/public/bundle.js
+++ tutoriales/Javascript/medio-avanzado/Proyectos/Tienda/public/bundle.js
@@ -78,7 +78,20 @@ botonMas.addEventListener('click', (e) => {
             - Quizas sea necesario pasarlo a entero
         - Obtengo el color a partir del id propiedad-color que tenga el valor seleccionado
         - Obtengo el tamaño igual que el color con el id propiedad-tamaño
-        - Esta información la agregaré al arreglo llamado carrito que contendrá un objeto con lo obtenido anteriormente
+        - Agrego esta información mediante push
+
+        - Renderizar el carrito:
+            - Activo la ventana.
+            - Recorro el carrito y en cada pasada
+                - Por cada uno de los elementos llamado productoCarrito
+                - Creo la plantilla.
+                    - Añado de forma dinámica lo obtenido en el paso anterior mediante backticks
+                - El precio lo cogeré de la base de datos simulada en productos.js
+
+
+
+            
+
 
 
 
@@ -109,7 +122,43 @@ const carrito = [];
 // Función que abrirá la ventana del carrito y revisa los productos agregados actualmente
 const renderCarrito = () => {
     ventanaCarrito.classList.add('carrito--active');
-    console.log(carrito);
+
+    carrito.forEach((productoCarrito) => {
+        const plantillaProducto = `
+            <div class="carrito__producto-info">
+                <img src="./img/tennis/1.jpg" alt="" class="carrito__thumb" />
+                <div>
+                    <p class="carrito__producto-nombre">
+                        <span class="carrito__producto-cantidad">${productoCarrito.cantidad} x </span>${productoCarrito.nombre}
+                    </p>
+                    <p class="carrito__producto-propiedades">
+                        Tamaño:<span>${productoCarrito.tamaño}</span> Color:<span>${productoCarrito.color}</span>
+                    </p>
+                </div>
+            </div>
+            <div class="carrito__producto-contenedor-precio">
+                <button class="carrito__btn-eliminar-item" data-accion="eliminar-item-carrito">
+                    <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="16"
+                        height="16"
+                        fill="currentColor"
+                        viewBox="0 0 16 16"
+                    >
+                        <path
+                            d="M2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2zm3.354 4.646L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 1 1 .708-.708z"
+                        />
+                    </svg>
+                </button>
+                <p class="carrito__producto-precio">$500.00</p>
+            </div>
+        `;
+
+        const itemCarrito = document.createElement('div');
+        itemCarrito.classList.add('carrito__producto');
+        itemCarrito.innerHTML = plantillaProducto;
+        ventanaCarrito.querySelector('.carrito__body').appendChild(itemCarrito);
+    });
 }; 
 
 // Recorro los dos botones que abren y cierran el carrito, les añado un event listener y muestro o oculto la ventana
