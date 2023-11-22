647c7e7f2d780f53bea470d6f697e7ae98a5e7f0
ServosoftOnline <oscarfernandezsantiago@gmail.com>
2023-11-22 14:01:27 +0100

    Proyecto react Lista de tareas: Añadiendo local storage

--- tutoriales/react/Practicas/lista_tareas/src/componentes/App.js
+++ tutoriales/react/Practicas/lista_tareas/src/componentes/App.js
@@ -5,12 +5,13 @@
     - Defino los siguiente estados:
 
       - El estado tareas que almacenará un arreglo compuesto por un objeto por cada tarea.
-        - Su valor inicial es un arreglo vacio
+        - Su valor inicial es un arreglo vacio.
+          - Al empezar a ejecutar localstorage cambie este valor inicial lo cambie por la cte tareasGuardadas
+
         - A medida que añadimos tareas se añadirán objetos
         - Este arreglo sería el que se almacenase en una base de datos, la nube o guardarlos en el navegador
-          - Para esto último Javascript usa local storage y será lo que haremos en esta app
-            - cada vez que cambie el estado tareas
-
+          - Para esto último Javascript usa local storage y describo como hacerlo al final de estas notas
+   
       - El estado mostrarCompletadas contendrá true o false
         - si contiene true mostrará solo las tareas completadas
         - Si contiene false las mostrará todas
@@ -31,8 +32,37 @@
           - Las tareas para que las muestre
           - Su funcion asociada para cambiarlas cuando queden completadas
 
-    - Uso de local storage
-      - Importar useEffect
+    - LOCAL STORAGE
+      - Es necesario importar la función useEffect
+      - Permite almacenar en el navegador informacion como texto y que no se pierda cuando se actualize la pagina
+      - Esta cadena de texto podremos verla en Inspeccionar/Almacenamiento/Almacenamiento local en nuestro navegador      
+
+      - Uso la función 
+        - Añadimos una dependencia con el array tareas.
+          - Con esto useEffect ejecutará el código que contiene al principio y cada vez que se actualize el estado de tareas
+          - Este codigo se encarga de gestionar la informacion que almacenemos
+            - El método setItem añade un elemento dentro de localStorage.
+              - Le indicamos el nombre de ese elemento y la cadena de texto a almacenar
+                - En nuestro caso el elemento será tareas
+                - La cadena de texto sera el array que contiene un objeto por cada tareas
+                  - Antes debemos transformar ese array en cadena de texto mediante JSON.stringify(elemento)
+
+            - El metodo getItem permite obtener un elemento almacenado en el navegador
+              - Lo obtenemos en una cadena de texto
+              - Si no hay nada almacenado devuelve null
+                - y tareasGuardadas almacenará un array vacio
+              - Si devuelve algo, transformo la cadena de texto a array mediante JSON.parse(Elemento obtenido)
+                - y lo almaceno en tareasGuardadas
+              
+                
+            
+            
+                
+
+
+
+
+
 
 
 */
@@ -44,27 +74,14 @@ import FormularioTareas from './FormularioTareas';
 import ListaTareas from './ListaTareas';
 
 const App = () => {
-  const [tareas,cambiarTareas] = useState(
-    [
-      /*
-      {
-        id:1,
-        texto: 'Lavar la ropa',
-        completada: true
-      },
-      
-      {
-        id:2,
-        texto: 'Grabar tutorial',
-        completada: false
-      }
-      */
-      
-    ]
-  );
 
+  const tareasGuardadas = localStorage.getItem('tareas') ? JSON.parse(localStorage.getItem('tareas')) : [] ;  
+  const [tareas,cambiarTareas] = useState(tareasGuardadas);
   const [mostrarCompletadas, cambiarMostrarCompletadas]= useState(false);
 
+  useEffect(() => {
+    localStorage.setItem('tareas', JSON.stringify(tareas));
+  }, [tareas]);
  
   return (
     <div className="contenedor">
