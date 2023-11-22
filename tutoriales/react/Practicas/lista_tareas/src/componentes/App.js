f11e7683281a166d89fb733c61badcf134500dd8
ServosoftOnline <oscarfernandezsantiago@gmail.com>
2023-11-22 23:18:47 +0100

    Fin del proyecto de react: Lista de tareas. Le apliqué local storage

--- tutoriales/react/Practicas/lista_tareas/src/componentes/App.js
+++ tutoriales/react/Practicas/lista_tareas/src/componentes/App.js
@@ -32,12 +32,15 @@
           - Las tareas para que las muestre
           - Su funcion asociada para cambiarlas cuando queden completadas
 
-    - LOCAL STORAGE
+    
+    - LOCAL STORAGE:
+
       - Es necesario importar la función useEffect
       - Permite almacenar en el navegador informacion como texto y que no se pierda cuando se actualize la pagina
       - Esta cadena de texto podremos verla en Inspeccionar/Almacenamiento/Almacenamiento local en nuestro navegador      
 
-      - Uso la función 
+      - Uso la función useEffect:
+
         - Añadimos una dependencia con el array tareas.
           - Con esto useEffect ejecutará el código que contiene al principio y cada vez que se actualize el estado de tareas
           - Este codigo se encarga de gestionar la informacion que almacenemos
@@ -53,17 +56,11 @@
                 - y tareasGuardadas almacenará un array vacio
               - Si devuelve algo, transformo la cadena de texto a array mediante JSON.parse(Elemento obtenido)
                 - y lo almaceno en tareasGuardadas
-              
-                
-            
-            
-                
-
-
-
-
-
-
+        
+        - Repito lo mismo para mostrarCompletadas
+          - El valor inicial del estado mostrarCompletadas es false
+          - Si lo cambiara en algun momento a true y refescara el navegador volvería a ser false
+          - Para pasar de booleano a string usaré toString()
 
 */
 
@@ -75,13 +72,29 @@ import ListaTareas from './ListaTareas';
 
 const App = () => {
 
+  // Obtengo las tareas guardadas en local storage y la asigno como valor por defecto en el estado tareas
   const tareasGuardadas = localStorage.getItem('tareas') ? JSON.parse(localStorage.getItem('tareas')) : [] ;  
   const [tareas,cambiarTareas] = useState(tareasGuardadas);
-  const [mostrarCompletadas, cambiarMostrarCompletadas]= useState(false);
-
+  
+  // Almaceno las tareas en local storage
   useEffect(() => {
     localStorage.setItem('tareas', JSON.stringify(tareas));
   }, [tareas]);
+
+  // Obtengo mostrar completadas de local storage. Si no hay ningun valor le asigno el valor deseado por defecto
+  // y si lo hay le asigno el valor booleano de comparar lo que obtuve y la cadena true.
+  // Que devolverá true si la cadena devuelta es true o false en caso contrario.
+  
+  let valorMostrarCompletadas = '';
+  if (localStorage.getItem('mostrarCompletadas' === null)) valorMostrarCompletadas = false;
+  else valorMostrarCompletadas = localStorage.getItem('mostrarCompletadas') === 'true';
+  
+  // Creo el estado de mostrar tareas completadas y lo almaceno en local storage
+  const [mostrarCompletadas, cambiarMostrarCompletadas]= useState(valorMostrarCompletadas);
+  useEffect(() => {
+    localStorage.setItem('mostrarCompletadas', mostrarCompletadas.toString());
+  }, [mostrarCompletadas]);
+
  
   return (
     <div className="contenedor">
