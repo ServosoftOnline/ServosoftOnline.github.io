e1b4ca22bfef54638fa1d4f7f9018d014db6eb61
servosoftonline <oscarfernandezsantiago@gmail.com>
2023-12-28 11:08:47 +0100

    Inicio proyecto firebase

---
+++ tutoriales/react/practicas/vite/contactosFirebase/src/App.jsx
@@ -0,0 +1,7 @@
+/*
+  LISTA DE CONTACTOS:
+  
+    - Aplicación que permitirá agregar, eliminar o editar contactos
+    - Se almacenará en una base de datos proporcionada por un servicio de google llamado firebase
+
+*/
---
+++ tutoriales/react/practicas/vite/contactosFirebase/src/main.jsx
@@ -0,0 +1,27 @@
+/*
+  FIREBASE:
+
+    - Permite ahorrar el aprender un lenguaje en el lado del servidor
+    - su web: https://firebase.google.com
+
+    - Proporciona varios servicios
+      - Usare cloud Firestore para las bases de datos
+      - Usaré Authentification para auntentificar usuarios
+      - cloud funtions proporciona funciones que podemos ejecutar en el lado del servidor
+      - hosting para publicar nuestro sitio en internet
+      - cloud storage para subir archivos
+    
+    - Tiene un plan gratuito y otro de pago por uso
+
+
+*/
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
