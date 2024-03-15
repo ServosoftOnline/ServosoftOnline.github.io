a9504c9f0933379e559edd8ec167f1358a5f186b
servosoftonline <oscarfernandezsantiago@gmail.com>
2024-03-15 17:55:08 +0100

    Continuo en la bibilioteca

--- tutoriales/react/practicas/WebpackBabel/webpack.config.js
+++ tutoriales/react/practicas/WebpackBabel/webpack.config.js
@@ -3,6 +3,7 @@
 
     - El archivo ha sido obtenido desde la documentacion en el sitio:
       - https://webpack.js.org/guides/getting-started/#using-a-configuration
+      - mode: 'development' tuve que añadirlo a posteriori
       - entry indica el archivo de entrada
       - output indica:
         - filename el nombre del archivo ya compilado con el resultado de la empaquetación
@@ -14,9 +15,26 @@
 const path = require('path');
 
 module.exports = {
+  mode: 'development',
   entry: './src/index.js',
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'public'),
   },
+
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env", "@babel/preset-react"]
+          }
+        }
+      }
+    ]
+  }
+
 };
